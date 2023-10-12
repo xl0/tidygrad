Fix log backprop, MNIST trains

tidygrad/tensor.py
@@ -58,11 +58,16 @@ class Tensor:
     op = "L"
     name: str = ""
     parents: list = []
+    eps = 1e-10
 
-    def __init__(self, data, name=""):
+    def __init__(self, data, name="", eps=1e-8):
+        if not isinstance(data, np.ndarray):
+            data = np.array(data)
+
         self.data = data
         self.name = name
         self.grad = np.zeros_like(self.data)
+        self.eps = eps
 
     def broadcast(self, target_shape):
         self_shape = self.data.shape
@@ -125,6 +130,11 @@ class Tensor:
         return out
 
     def log(self, name="Noname"):
+        # data = self.data
+        # print(f"min/max of data is {data.min()}/{data.max()}")
+        # print(f"min/max of res is {np.log(data).min()}/{np.log(data).max()}")
+        # print(f"Log of:\n{data}\nis:{np.log(data)}")
+
         out = LogTensor(data=np.log(self.data), name=name)
         out.parents = [self]
         return out
@@ -158,6 +168,8 @@ class Tensor:
         # Start with the current node
         visited = []
         nodes = []
+
+        assert self.data.size == 1, "Cannot call backward on non-scalar tensor"
 
         def walk(node):
             for p in node.parents:
@@ -252,7 +264,7 @@ class LogTensor(Tensor):
     op = "log"
 
     def _backward(self):
-        self.parents[0].grad += self.grad * (1 / self.data)
+        self.parents[0].grad += self.grad * (1.0 / (self.parents[0].data))
 
 
 class BroadcastTensor(Tensor):

tidygrad/utils.py
@@ -8,29 +8,47 @@ from .tensor import Tensor
 import numpy as np
 
 # %% ../nbs/00_utils.ipynb 4
-def grad_check(nn, x, y, params: tuple, eps=1e-7):
+def grad_check(nn, inputs, params: tuple = (), eps=1e-5, n=1000):
     for p in reversed(params):
         # Reshape to 1D so it's easier to sample random indices
 
         data_view = p.data.reshape(-1)
         grad_view = p.grad.reshape(-1)
 
-        slow_grad_view = np.zeros_like(p.grad).reshape(-1)
+        slow_grad = np.zeros_like(p.grad)
+        slow_grad_view = slow_grad.reshape(-1)
 
-        indices = np.random.choice(np.arange(len(grad_view)), size=1000)
+        indices = np.random.choice(
+            np.arange(grad_view.size), size=min(n, grad_view.size), replace=False
+        )
+        indices = list(filter(lambda idx: abs(grad_view[idx]) > 1e-9, indices))
         for idx in indices:
-            loss = nn(x, y, params)
-
             old_val = data_view[idx]
-            data_view[idx] = old_val + eps
 
-            loss_plus_h = nn(x, y, params)
+            loss = nn(inputs, params)
+
+            data_view[idx] = old_val + eps
+            loss_plus_h = nn(inputs, params)
+            # print(f"loss_plus_h: {loss_plus_h.data}")
+            # print(f"loss: {loss.data}")
+            # print(f"diff: {loss_plus_h.data - loss.data}")
+            # print(f"derivative: {(loss_plus_h.data - loss.data) / eps}")
 
             slow_grad_view[idx] = (loss_plus_h.data - loss.data) / eps
             data_view[idx] = old_val
-        max_grad_diff = np.max(np.abs(slow_grad_view[indices] - grad_view[indices]))
 
-        if max_grad_diff > 1e-4:
+        max_grad_diff = np.max(
+            np.abs(
+                (slow_grad_view[indices] - grad_view[indices])
+                / np.maximum(slow_grad_view[indices], grad_view[indices])
+            )
+        )
+
+        print(f"Max gradient difference for {p.name}: {max_grad_diff*100}%")
+        # print(f"Gradient : {p.grad}")
+        # print(f"Slow grad: {slow_grad_view}")
+        # print(f"Diff: {np.where(slow_grad_view, slow_grad_view - grad_view, 0)}")
+        if max_grad_diff > 1e-2:
             raise ValueError(
                 f"Gradient check failed for {p.name}: Max error: {max_grad_diff}"
             )